c498a936c2d128f983263e03fca9c3ff2f64152a
sohelhussain <sohelhussaing@gmail.com>
2024-11-15 08:30:57 +0530

    wip: STRUCTURE

--- scripts/script.js
+++ scripts/script.js
@@ -352,3 +352,64 @@ function page1() {
   
 }
 // page1()
+
+
+
+
+// ! container first
+
+
+const firstAnimation = () => {
+  const swip = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#container-first",
+      scroller: "body",
+      markers: true,
+      start: "0% 0%",
+      end: "100% -100%",
+      pin: true,
+      scrub: 1,
+    },
+  });
+  swip
+    .to(
+      `.row-div`,
+      {
+        marginTop: "-30%",
+      },
+      "sam"
+    )
+    .to(
+      `.rotate-div`,
+      {
+        transform: `rotate(-20deg) scale(.8)`,
+        delay: 0.2,
+        marginLeft: "10%",
+      },
+      "sam"
+    )
+    .to(
+      `.containt-part1 h1`,
+      {
+        opacity: 1,
+        // display: "block",
+        delay: 0.1,
+      },
+      "sam"
+    )
+    .to(
+      `.overlaye`,
+      {
+        backgroundColor: `rgba(0, 0, 0, 0.416)`,
+      },
+      "sam"
+    )
+    .to(
+      `.inner-line`,
+      {
+        width: "100%",
+      },
+      ""
+    );
+};
+// firstAnimation();
